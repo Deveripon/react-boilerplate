7a4a36ce72c7a235ef86c5b715fca2603dee1858
devripon <devripon@devripon.io>
2023-12-07 23:33:22 +0600

    react boilerplate for immidiet effect

---
+++ src/pages/Auth/Auth.jsx
@@ -0,0 +1,17 @@
+import Meta from "../../components/Meta/Meta";
+const Auth = () => {
+    return (
+        <>
+            <Meta>
+                <title>Boilerplate</title>
+                <link
+                    rel="shortcut icon"
+                    href="https://static.xx.fbcdn.net/rsrc.php/yb/r/hLRJ1GG_y0J.ico"
+                    type="image/x-icon"
+                />
+            </Meta>
+        </>
+    );
+};
+
+export default Auth;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                helvetica: ["helvetica", "sans"],
+            },
+            colors: {
+                primary: {
+                    DEFAULT: "#1877f2",
+                    hover: "#365899",
+                },
+                secondary: {
+                    DEFAULT: "#42b72a",
+                    hover: "#36a420",
+                },
+                linkColor: "#1877f2",
+                secondaryText: "#606770",
+            },
+        },
+    },
+    plugins: [],
+};
